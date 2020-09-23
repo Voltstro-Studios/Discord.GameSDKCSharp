7e8aee537b9a3f0bde84ccd2db13208f9c98100f
Voltstro <me@voltstro.dev>
2020-09-23 14:36:26 +1000

    Added base project stuff

---
+++ src/Discord.GameSDK.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Discord.GameSDK.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
